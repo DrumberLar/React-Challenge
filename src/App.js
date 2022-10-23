848b00964d5428528e50ad54d05dc77ad7c3aa90
Drumber <95851621+DrumberLar@users.noreply.github.com>
2022-10-24 02:29:27 +0300

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+const App = () => {
+  const [color, setColor] = useState('');
+  const [answers, setAnswers] = useState([]);
+  const [textOutput, setTextOutput] = useState('Find the correct answer');
+  const generateRandomColor = () => {
+    let maxVal = 0xffffff; // 16777215.
+    let randomNumber = Math.random() * maxVal;
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6, 0);
+    const randomColor = `#${randColor.toUpperCase()}`;
+    return randomColor;
+  };
+
+  const displayRandomBoxes = () => {
+    return answers.map((answer) => {
+      return (
+        <button
+          className="color-box"
+          key={answer}
+          onClick={() => {
+            if (answer === color) {
+              setTextOutput('Correct');
+              generateAnswersAndColor();
+            } else {
+              setTextOutput('Wrong!Keep Trying!');
+            }
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {answer}
+        </button>
+      );
+    });
+  };
+  const generateAnswersAndColor = () => {
+    const correctColor = generateRandomColor();
+    setColor(correctColor);
+    setAnswers(
+      [correctColor, generateRandomColor(), generateRandomColor()].sort(
+        () => 0.5 - Math.random()
+      )
+    );
+  };
+  useEffect(() => {
+    generateAnswersAndColor();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div className="container">
+      <div className="App" style={{ background: color }}></div>
+
+      <div className="color-boxes">{displayRandomBoxes()}</div>
+      <div>{color}</div>
+      <div>{textOutput}</div>
     </div>
   );
-}
+};
 
 export default App;
